a60cbf3bd3a7704517bfa9b35a070aa0ccb8a1c6
meejumaharjan <meejum18@gmail.com>
2023-02-27 23:00:56 -0500

    "This is an example of add"

---
+++ src/Add.java
@@ -0,0 +1,14 @@
+public class Add {
+
+    public static void main(String[] args) {
+        System.out.println("Examples of Variables");
+
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+        System.out.println("Sum is " + sum);
+        String text = ("Today is my 2nd day in Tech Motion Learning");
+        System.out.println(text + "\n" + sum);
+    }
+}
